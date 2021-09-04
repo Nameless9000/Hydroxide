4f6a1135925ed95e6b82799e893fac2ea86cca29
Nameless <Nameless9000@protonmail.com>
2021-09-04 19:34:28 +0100

    Update main.lua

--- ui/main.lua
+++ ui/main.lua
@@ -1,4 +1,4 @@
-local CoreGui = game:GetService("CoreGui")
+local CoreGui = game.Players.LocalPlayer.PlayerGui.PhoneUI
 local UserInput = game:GetService("UserInputService")
 local HttpService = game:GetService("HttpService")
 
